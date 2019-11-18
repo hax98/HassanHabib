07c7ad132a3e126c06427fe29325c00be4337962
hax98 <habib_hassan@hotmail.it>
2019-11-18 20:00:14 +0100

    Versione della libreria per invio e ricezione sms con app di test annessa.

---
+++ app/src/main/java/com/example/smsapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.smsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    final int SEND_SMS_PERMISSION_REQUEST_CODE=1;
+    EditText message;
+    EditText number;
+    Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number = findViewById(R.id.inputnumber);
+        message = findViewById(R.id.inputmessage);
+        send = findViewById(R.id.sendbutton);
+
+        send.setEnabled(false);
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            send.setEnabled(true);
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},SEND_SMS_PERMISSION_REQUEST_CODE);
+        }
+
+    }
+    public void onSend(View V){
+        String phonenumber = number.getText().toString();
+        String smsmessage = message.getText().toString();
+
+        if(phonenumber == null || smsmessage == null || phonenumber.length()==0 || smsmessage.length()==0){
+            return;
+        }
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            SmsManager smsManager= SmsManager.getDefault();
+            smsManager.sendTextMessage(phonenumber,null,smsmessage,null,null);
+            Toast.makeText(this,"message sent",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"permissions denied",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public boolean checkPermission (String permission){
+        int check = ContextCompat.checkSelfPermission(this,permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
+    }
+}
+
+
+
